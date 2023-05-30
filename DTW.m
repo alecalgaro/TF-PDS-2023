fc0dcc5ae25ab26fa1d989888a790eb2bef77540
AE3998 <nehc.nitsuga@gmail.com>
2023-05-30 19:03:36 -0300

    DTW optimizado y es capaz de buscar el camino de regreso con los indices i y j

--- DTW.m
+++ DTW.m
@@ -1,56 +1,84 @@
-function dist = DTW(a, b)
+% Funcion DTW que tiene como entrada
+% a = vector de tamano (1, n)
+% b = vector de tamano (1, m)
 
-  length_a = length(a);
-  length_b = length(b);
 
-  an = sparse(zeros(length_a+1,length_b+1));
+clear; clc
+a = [2 3 3 4 5 4];
+b = [2 3 4];
 
-  # Inicializar los bordes
-  an(1, :) = 1e5;
-  an(:, 1) = 1e5;
+a = [1 1 2 3 4 4 5 6];
+b = [1 2 2 2 3 4 5 5 6 6];
 
-  an(1,1)=0;
 
-  # Tamano de ventana
-  w=abs(length_a - length_b);
+##fm = 30;
+##t1 = 0: 1/fm: 1 - 1/fm;
+##t2 = 0: 1/fm: 0.5;
+##a = sin(2*pi*(2 + 2*t1) .* t1);
+##b = sin(2*pi*5*t2);
+##
+##plot(t1, a);
+##hold on;
+##plot(t2, b);
 
-  for i = 1: length_a
-##      for j = max(1,i-w-3): min(length_b,i+w)
-      for j = 1: length_b
-          cost = abs(a(i) - b(j));
-          an(i+1, j+1) = cost + min([an(i,j+1),an(i+1,j),an(i,j)]);
-      end
-  end
 
-  dist = double(an(end,end));
+
+function [dist, an, idx_ij] = DTW(a, b)
+
+  if (size(a, 1) != 1 || size(b, 1) != 1)
+    printf("Dimension de las entradas erronea!")
+    dist = NaN;
+    return
+  endif
+
+  # Busqueda de las distnacias
+  an = abs(a' - b);
+  an(:, 1) = cumsum(an(:, 1));
+  an(1, :) = cumsum(an(1, :));
+
+  for i = 1: length(a) - 1
+      for j = 1: length(b) - 1
+          an(i+1, j+1) += min([an(i,j+1), an(i+1,j), an(i,j)]);
+      endfor
+  endfor
+
+  dist = an(end,end);
+
+
+  # Camino minimo con los indices de regreso
+  idx_ij = [length(a);
+            length(b)];
+  aux = [0, 1, 1;
+         1, 0, 1];
+
+  # Mientras ni i ni j llegase a 1, esto va regresando y guardando los
+  # indices de i y j en una matriz
+  while(idx_ij(1, end) != 1 && idx_ij(2, end) != 1)
+   i = idx_ij(1, end); j = idx_ij(2, end);
+
+   [val, x] = min([an(i,j-1), an(i-1,j), an(i-1,j-1)]);
+   idx_ij = [idx_ij, idx_ij(:, end) - aux(:, x)];
+
+  endwhile
+
+  # Comprueba si realmente llegaron a 1, sino agrega los numeros.
+  # Por ejemplo i = [5 4 3], j = [3 2 1], entonces rellena a
+  # i = [5 4 3 2 1], j = [3 2 1 1 1]
+
+  if (idx_ij(1, end) != 1)
+      cant = idx_ij(1, end) - 1;
+      idx_ij = [idx_ij, [cant: -1 :1; ones(1, cant)]];
+  elseif(idx_ij(2, end) != 1)
+      cant = idx_ij(2, end) - 1;
+      idx_ij = [idx_ij, [ones(1, cant); cant: -1: 1]];
+  endif
+
+
 
 endfunction
 
 
-clear; clc
-fm = 200;
-t1 = 0: 1/fm: 1 - 1/fm;
-t2 = 0: 1/fm: 0.5;
-a = sin(2*pi*(5 + 5*t1) .* t1);
-b = sin(2*pi*25*t2);
-
-
-plot(t1, a);
-hold on;
-plot(t2, b);
-
-##a = [2 3 3];
-##b = [2 3];
-
-
-dist = DTW(a, b)
-# Primer for
-# (1, 1) -> 0.4533
-
-# Segundo for j = 1: algo - 3
-##  (1, 1) -> 4.2274
-
-# Tercer for todo
+[dist, an, idx_ij] = DTW(a, b)
 
 
 
