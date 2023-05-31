ccc2f1afc8e423314f517b93b18387c06f0e4736
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-05-31 18:02:19 -0300

    Pruebas de correlación cruzada entre dos señales

--- TDF.m
+++ TDF.m
@@ -9,11 +9,6 @@ function [S,f] = TDF(s,tm)
   % La funcion fft solo nos realiza la sumatoria de los prod int.
   S = abs(fft(s));
   % Acomodamos S para que la segunda mitad (parte negativa) pase al principio:
-<<<<<<< HEAD
 ##  S = [S(N/2+1:N) S(1:N/2)];
   S = [S(round(N/2+1):end) S(1:round(N/2))];
 endfunction  
-=======
-  S = [S(round(N/2+1):end) S(1:round(N/2))];
-endfunction
->>>>>>> fc0dcc5ae25ab26fa1d989888a790eb2bef77540
