0a2b4b3d76fe7c2678d9b533b60b98bc535eb411
AE3998 <nehc.nitsuga@gmail.com>
2023-05-31 23:43:49 -0300

    Filtrar_senal agregado. La funcion te permite filtrar las ffrecuencias en forma pasa-banda, donde uno puede pasar la f_min y f_max

--- filtrar_senal.m
+++ filtrar_senal.m
@@ -1,11 +1,33 @@
-
+% Entradas
+% s = senal de entrada
+% fm = frecuencia de muestreo
+% f_min = frecuencia minima que desea dejar pasar
+% f_max = frecuencia maxima que desea dejar pasar
 
 function senal_filtrada = filtrar_senal(s, fm, f_min, f_max)
-  N = length(s);
-
   TDF_s = fft(s);
+  N = length(s);
+  df = fm/N;
 
+  # Despejar el numero de muestra de frecuencia minima/ maxima
+  idx_f_min = floor(f_min/df);
+  idx_f_max = ceil(f_max/df);
+
+  # Filtrar las frecuencias
+  TDF_s_filtrado = sparse(zeros(1, N));
+
+  TDF_s_filtrado(idx_f_min + 1: idx_f_max + 1) = ...
+           TDF_s(idx_f_min + 1: idx_f_max + 1);
+
+  TDF_s_filtrado(N - idx_f_max: N - idx_f_min) = ...
+           TDF_s(N - idx_f_max: N - idx_f_min);
+
+  senal_filtrada = real(ifft(TDF_s_filtrado));
+
+  # Mostrar resultado del filtro
+##  plot(abs(TDF_s));
+##  figure
+##  plot(abs(TDF_s_filtrado));
 
 endfunction
-
 
