1ec7b64a24616e4ceac0d3e7556567591cb720bc
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-06-06 11:42:57 -0300

    Probar con nuevos audios para comando detener

--- usuario_un_audio.m
+++ usuario_un_audio.m
@@ -11,7 +11,8 @@ clear;  clc;  close all;
   [data_atras] = load("data_atras.txt");
   [data_derecha] = load("data_derecha.txt");
   [data_izquierda] = load("data_izquierda.txt");
-  [data_parar] = load("data_parar.txt");
+  [data_detener] = load("data_detener.txt");
+##  [data_parar] = load("data_parar.txt");
 
 ##---------------------------------------------------------------------------
 ##  SE ANALIZA EL AUDIO DE ENTRADA
@@ -26,16 +27,17 @@ clear;  clc;  close all;
 
   ## Se busca el comando con mayor similitud a la senal de entrada y si se supera
   ## cierto umbral de similitud se indica como comando valido.
+  disp("")
   dif_mismo = DTW(mfcc, mfcc)
 
   dif_adelante = DTW(mfcc, data_adelante)
   dif_atras = DTW(mfcc, data_atras)
   dif_derecha = DTW(mfcc, data_derecha)
   dif_izquierda = DTW(mfcc, data_izquierda)
-  dif_parar = DTW(mfcc, data_parar)
+  dif_detener = DTW(mfcc, data_detener)
   disp("")
   
-  [res, i] = min([dif_adelante, dif_atras, dif_derecha, dif_izquierda, dif_parar]);
+  [res, i] = min([dif_adelante, dif_atras, dif_derecha, dif_izquierda, dif_detener]);
   res
 
 ##---------------------------------------------------------------------------
@@ -45,7 +47,7 @@ clear;  clc;  close all;
   ## Mostrar si la senial de entrada fue un comando valido o no, verificando si
   ## supera cierto umbral de similitud, e indicar cual fue el comando.
   
-  comando = [" adelante"; " atras"; " derecha"; " izquierda"; " parar"];
+  comando = [" adelante"; " atras"; " derecha"; " izquierda"; " detener"];
   
   umbral = 0;
   if(res > umbral)
